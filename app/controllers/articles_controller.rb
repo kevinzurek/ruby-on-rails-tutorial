bf1cf6c5a8d82d02769d111c508686347071a278
Kevin Zurek <kevinzurek@gmail.com>
2018-05-03 21:07:09 -0400

    finish rails tutorial section 5

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,22 @@
 class ArticlesController < ApplicationController
+
+  # Prefix: welcome_index
+  # Verb: GET
+  # Pattern: /welcome/index(.:format)
   def index
     @articles = Article.all
   end
 
+  # Prefix: article
+  # Verb: GET
+  # Pattern: /articles/:id(.:format)
   def show
     @article = Article.find(params[:id])
   end
 
-  def new
-    @article = Article.new
-  end
-
+  # Prefix: articles
+  # Verb: POST
+  # Pattern: /articles(.:format)
   def create
     @article = Article.new(article_params)
 
@@ -24,8 +30,43 @@ class ArticlesController < ApplicationController
 
   end
 
-  private
 
+
+  # Prefix: new_article
+  # Verb: GET
+  # Pattern: /articles/new(.:format)
+  def new
+    @article = Article.new
+  end
+
+  # Prefix: edit_article
+  # Verb: GET
+  # Pattern: /articles/:id/edit(.:format)
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  # Prefix: edit_article
+  # Verb: DELETE
+  # Pattern: /articles/:id(.:format)
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
   def article_params
     params.require(:article).permit(:title, :text)
   end
